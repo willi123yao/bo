b51e169b8dd1e49d5537bebeb5b98c9e83d0788d
aetheryx <zain1@live.nl>
2018-06-08 05:15:56 +0200

    make db.js slightly less shit

--- src/db.js
+++ src/db.js
@@ -1,7 +1,10 @@
 const { MongoClient } = require('mongodb');
 
 module.exports = async function getDB () {
-  const client = await MongoClient.connect('mongodb://127.0.0.1:27017')
+  const db = await MongoClient.connect('mongodb://127.0.0.1:27017')
+    // bo uses the verification helper's database because incidents were originally a dblvh feature
+    // todo: merge dblvh and bo or seperate database instances
+    .then(client => client.db('dblvh'))
     .catch(e => {
       if (e.message.includes('ECONNREFUSED')) {
         console.error(`Failed to connect to MongoDB: ${e.message}\nExiting...`, 'error');
@@ -9,5 +12,49 @@ module.exports = async function getDB () {
       }
     });
 
-  return client.db('dblvh');
+  const [ prefixes, incidents, timedActions ] = [ 'prefixes', 'incidents', 'timedActions' ].map(table => db.collection(table));
+
+  // every single db method used in the whole project is below
+  // todo: split tables into files
+  return {
+    async getPrefix (id) {
+      const res = await prefixes.findOne({ id });
+
+      return res
+        ? res.prefix
+        : this.config.defaultPrefix;
+    },
+
+    async setPrefix (id, prefix) {
+      const existingPrefix = await prefixes.findOne({ id });
+      if (existingPrefix) {
+        return prefixes.updateOne({ id }, { $set: { prefix } });
+      } else {
+        return prefixes.insertOne({ id, prefix });
+      }
+    },
+
+    getIncidentsByID (id) {
+      return incidents
+        .find({ id })
+        .sort({ t: -1 })
+        .toArray();
+    },
+
+    getIncidentByCase (caseNumber) {
+      return incidents.findOne({ n: caseNumber });
+    },
+
+    createTimedAction (options) {
+      return timedActions.insertOne(options);
+    },
+
+    getExpiredTimedActions () {
+      return timedActions.find({
+        expiryDate: {
+          $lt: Date.now()
+        }
+      }).toArray();
+    }
+  };
 };
