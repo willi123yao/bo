45e44ea8729befb4a734dbbb3368ce73a59fc6da
aetheryx <zain1@live.nl>
2018-06-08 17:11:03 +0200

    oops

--- src/events/onMessageCreate/index.js
+++ src/events/onMessageCreate/index.js
@@ -10,7 +10,7 @@ module.exports = async function onMessageCreate (msg) {
   if (
     msg.channel.guild.id !== this.config.serverID ||
     msg.author.bot ||
-    !msg.member.permissions.has('banMembers')
+    !msg.member.permission.has('banMembers')
   ) {
     return;
   }
