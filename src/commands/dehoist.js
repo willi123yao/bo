8d40602361cb8e03ce4eba896f48c8a279aa19bb
aetheryx <zain1@live.nl>
2018-06-08 05:18:09 +0200

    fix dehoist

--- src/commands/dehoist.js
+++ src/commands/dehoist.js
@@ -1,29 +1,27 @@
-// const sleep = (ms) => new Promise(r => setTimeout(r, ms));
+const sleep = (ms) => new Promise(r => setTimeout(r, ms));
 
 module.exports = class DehoistCommand {
   async execute ({ msg: { channel, author }, client: { dehoist } }) {
-    return 'do it urself bitch';
+    await channel.guild.fetchAllMembers();
+    const hoisters = channel.guild.members.filter(u => {
+      const username = u.nick || u.username;
+      return username[0].localeCompare('0') === -1;
+    });
 
-    // await channel.guild.fetchAllMembers();
-    // const hoisters = channel.guild.members.filter(u => {
-    //   const username = u.nick || u.username;
-    //   return username.toLowerCase().codePointAt(0) < 48; // codepoint of '0'
-    // });
+    if (!hoisters[0]) {
+      return 'No hoisters to de-hoist.';
+    }
 
-    // if (!hoisters[0]) {
-    //   return 'No hoisters to de-hoist.';
-    // }
+    channel.createMessage(`Dehoisting ${hoisters.length} users.`);
+    for (const hoister of hoisters) {
+      hoister.edit({ nick: 'bad hoister' })
+        .catch(e =>
+          channel.createMessage(`Failed to dehoist ${hoister.username} (\`${hoister.id}\`): ${e.message}`)
+        );
+      await sleep(1100);
+    }
 
-    // channel.createMessage(`Dehoisting ${hoisters.length} users.`);
-    // for (const hoister of hoisters) {
-    //   dehoist(hoister)
-    //     .catch(e =>
-    //       channel.createMessage(`Failed to dehoist ${hoister.username} (\`${hoister.id}\`): ${e.message}`)
-    //     );
-    //   await sleep(1100);
-    // }
-
-    // return `Finished, ${author.mention}.`;
+    return `Finished, ${author.mention}.`;
   }
 
   get props () {
--- src/events/onGuildMember.js
+++ src/events/onGuildMember.js
@@ -1,5 +1,5 @@
 module.exports = async function onGuildMember (_, member) {
-  if ((member.nick || member.username).toLowerCase().codePointAt(0) < 48) { // codepoint of '0'
-    this.dehoist(member);
+  if ((member.nick || member.username).toLowerCase()[0].localeCompare('0') === -1) {
+    member.edit({ nick: 'bad hoister' });
   }
 };
